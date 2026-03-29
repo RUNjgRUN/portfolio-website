f868b6dc95310743b51cdae8ff5990aa99bd2802
Jack Gar <koshanekoshane36@gmail.com>
2026-03-29 13:20:17 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll("nav a");
+
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+  const linkPage = link.getAttribute("href");
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
